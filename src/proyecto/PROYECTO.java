40c201dc3f8efbf5d8c09e4fa516f49c484af2a4
UrielParedes05 <142622188+UrielParedes05@users.noreply.github.com>
2023-11-15 16:07:50 -0500

    Add files via upload

--- src/proyecto/PROYECTO.java
+++ src/proyecto/PROYECTO.java
@@ -1,22 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto;
 
 import Ventanas.VENT_LOGIN;
 
-/**
- *
- * @author ESTUDIANTE
- */
 public class PROYECTO {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         VENT_LOGIN inicio = new VENT_LOGIN();
     }
     
